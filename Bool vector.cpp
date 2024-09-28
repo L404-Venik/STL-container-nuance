ae18c876ec2ea9e4d598feea0dd105d2cdf14092
Strokov Veniamin <67225529+L404-Venik@users.noreply.github.com>
2024-09-28 16:54:24 +0300

    Add files via upload

---
+++ Bool vector.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<bool> v(10, true);
+    for (auto&& e : v)
+    {
+        std::cout << e << std::endl;
+        e = false;
+    }
+
+    // cycle below does not compile because of std::vector<bool> special optimization
+    // https://en.cppreference.com/w/cpp/container/vector_bool
+    for (auto& e : v)
+    {
+        std::cout << e << std::endl;
+    }
+}
